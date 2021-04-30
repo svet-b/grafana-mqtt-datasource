a60f72af3aaec43105c8bec6102630d0596cb628
Svet Bajlekov <tomatoman@gmail.com>
2021-04-29 22:04:55 -0400

    Enable parsing of different data types + JSON traversal

--- src/QueryEditor.tsx
+++ src/QueryEditor.tsx
@@ -1,10 +1,12 @@
 import defaults from 'lodash/defaults';
 
 import React, { ChangeEvent, PureComponent } from 'react';
-import { InlineFieldRow, InlineField, Input, Button } from '@grafana/ui';
-import { QueryEditorProps } from '@grafana/data';
+import { InlineFieldRow, InlineField, Select, Input, Button } from '@grafana/ui';
+import { QueryEditorProps, SelectableValue } from '@grafana/data';
 import { DataSource } from './datasource';
 import { defaultQuery, MyDataSourceOptions, MyQuery } from './types';
+
+import { DataType, DataTypeValue, ValueEncoding } from './constants';
 
 type Props = QueryEditorProps<DataSource, MyQuery, MyDataSourceOptions>;
 
@@ -19,11 +21,25 @@ export class QueryEditor extends PureComponent<Props> {
     onChange({ ...query, [name]: event.target.value });
   };
 
+  /**
+   * Change handler for select field
+   *
+   * @param {ChangeEvent<HTMLInputElement>} event Event
+   */
+  createSelectFieldHandler<ValueType>(name: keyof MyQuery) {
+    return (val: SelectableValue<ValueType>) => {
+      this.props.onChange({ ...this.props.query, [name]: val.value });
+    };
+  }
+
   onTopicChange = this.createTextFieldHandler('topic');
+  onDataTypeChange = this.createSelectFieldHandler<string>('dataType');
+  onDataPathChange = this.createTextFieldHandler('dataPath');
+  onValueEncodingChange = this.createSelectFieldHandler('valueEncoding');
 
   render() {
     const query = defaults(this.props.query, defaultQuery);
-    const { topic } = query;
+    const { topic, dataType, dataPath, valueEncoding } = query;
     const { onRunQuery } = this.props;
 
     return (
@@ -40,6 +56,33 @@ export class QueryEditor extends PureComponent<Props> {
             Subscribe
           </Button>
         </InlineFieldRow>
+        <InlineFieldRow>
+          <InlineField labelWidth={20} label="Data type">
+            <Select options={DataType} value={dataType} width={40} onChange={this.onDataTypeChange} />
+          </InlineField>
+        </InlineFieldRow>
+        {dataType === DataTypeValue.JSON && (
+          <InlineFieldRow>
+            <InlineField
+              labelWidth={20}
+              label="Data path"
+              tooltip="Path of value in JSON object, parsed using lodash _get()"
+            >
+              <Input value={dataPath || ''} width={80} placeholder="a.b[0]" onChange={this.onDataPathChange} />
+            </InlineField>
+          </InlineFieldRow>
+        )}
+        {dataType === DataTypeValue.BYTES && (
+          <InlineFieldRow>
+            <InlineField
+              labelWidth={20}
+              label="Value envoding"
+              tooltip="How the numerical value is encoded as a binary payload"
+            >
+              <Select options={ValueEncoding} value={valueEncoding} width={40} onChange={this.onValueEncodingChange} />
+            </InlineField>
+          </InlineFieldRow>
+        )}
       </>
     );
   }
